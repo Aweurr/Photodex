37772b7d4e34958b2e8428ea82ed907f40ab5ec9
Logan LB <aweurdev@gmail.com>
2025-04-05 18:30:48 +0200

    LocalStorage + CSS

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,12 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./ColorPicker.css";
 
 const ColorPicker = ({ currentColor, onChange }) => {
   const colors = ["#FFB3C1", "#FFD6A5", "#FDFFB6", "#CAFFBF", "#A0C4FF"]; // Tons pastel
   const [isOpen, setIsOpen] = useState(false);
+  const pickerRef = useRef(null); // Référence sur le composant
+
+  // Fermer le colorpicker si clic à l'extérieur
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (pickerRef.current && !pickerRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
 
   return (
-    <div className="color-picker">
+    <div ref={pickerRef} className="color-picker">
       <div
         className="color-display"
         style={{ backgroundColor: currentColor }}
